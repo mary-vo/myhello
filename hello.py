355c9babe60a318613adf01c3e44c2e7e3af9619
Mary Vo <maryvo.uk@gmail.com>
2023-01-17 20:55:45 -0500

    enhance hello.pyy make FN with return - test success

--- hello.py
+++ hello.py
@@ -8,5 +8,13 @@ def main():
 
 print("Special Hello World!")
 
+def print_msg(msg_txt): 
+    print(f"{msg_txt}") 
+    return 0
+
+mymsg = "Hello World!"
+
+return_code = print_msg(mymsg)
+
 if __name__ == '__main__': 
     main()
--- test_hello.py
+++ test_hello.py
@@ -1,3 +1,7 @@
 import hello
 
 hello.print_hello()
+
+return_code = hello.return_code
+
+print(f"return_code:{return_code}")
