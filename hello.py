5d78cc4b6e5d4255fee2bcef8050b081e725c17a
Mary Vo <maryvo.uk@gmail.com>
2023-01-17 20:49:04 -0500

    enhance hello.py with FN - test success

--- hello.py
+++ hello.py
@@ -1,1 +1,12 @@
 print("Hello World!")
+
+def print_hello(): 
+    print("Hello World!")
+
+def main(): 
+    print_hello()
+
+print("Special Hello World!")
+
+if __name__ == '__main__': 
+    main()
